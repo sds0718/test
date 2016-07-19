86ea13f2c6287682a5ec79686c7d0aa50d9479f6
sds0718 <sds_lss20151108@163.com>
2016-07-19 10:41:06 +0800

    提交de.php文件

---
+++ de.php
@@ -0,0 +1,29 @@
+<?php
+header("Content-type:text/html;charset=utf-8");
+if($_POST){
+  if($_POST['username']==""){
+    echo"用户名不能为空";
+    exit;
+  }
+  if($_POST['password']==""){
+    echo"密码不能为空";
+    exit;
+  }
+
+}
+
+
+?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Insert title here</title>
+</head>
+<body>
+ <form action="#" method="post">
+ 用户名<input type="text" name="username"/><br/>
+ 密码  <input type="text" name="password"/><br/>
+       <input type="submit" value="提交">
+ </form>
+</body>
+</html>
